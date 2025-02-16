e1b92edc77adf19689c8ba0c000f3f369c6d2998
wesleylearn <wesley.duartepb@gmail.com>
2025-02-16 13:00:34 -0300

    update

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -3,18 +3,15 @@ import { ObjectId } from "mongodb";
 export interface UserDetailsDto {
   id: ObjectId | undefined;
   name: string;
-  image?: string;
   email: string;
 }
 
 export interface UserSummaryDto {
   id: ObjectId | undefined;
   name: string;
-  image?: string;
 }
 
 export interface UserUpdateDto {
   name?: string;
-  image?: string;
   email?: string;
 }
